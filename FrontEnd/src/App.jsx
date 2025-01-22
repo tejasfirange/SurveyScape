045e6bc6fe8d8371cfa1cc3d16dabf3625880562
Sairaj <ekbotesairaj640@gmail.com>
2025-01-22 06:07:50 +0530

    Adding mag effect

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <Adlogin />
-      {/* <Landing/> */}
+      <Landing/>
       
     </div>
   )
--- FrontEnd/src/components/Admin/Adlogin.jsx
+++ FrontEnd/src/components/Admin/Adlogin.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Adlogin() {
-  return (
-    <div className='main w-full flex items-center justify-center h-screen bg-[#1c1c1c]'>
-        <div id='card' className='relative rounded-[50px] w-[90%] h-[85%] lg:w-[25%] lg:h-[65%] bg-[#c4c2c22b]'>
-            <div id='cir1' className='hidden lg:block w-[150px] h-[150px]  absolute top-[-50px] left-[-50px] rounded-[50%]'>
 
-            </div>
-            <div id='cir1' className='hidden lg:block w-[150px] h-[150px]  absolute bottom-[-50px] right-[-50px] rounded-[50%]'>
+  useEffect(()=>{
+    window.addEventListener('mousemove', function(e) {
+      gsap.to('#cur', {
+        x: e.clientX,
+        y: e.clientY,
+        duration: 0.3
+      });
+    });
+  
+    document.querySelector("#but").addEventListener('mouseenter', function(){
+      gsap.to('#cur', {
+        width: '60px',
+        height: '60px',
+        delay: .3
+      })
+      gsap.to("#but",{
+        fontSize : '40px',
+        delay: .3
+      })
+    })
+    document.querySelector("#but").addEventListener('mouseleave', function(){
+      gsap.to('#cur', {
+        width: '30px',
+        height: '30px',
+        delay: .3
+      })
+      gsap.to("#but",{
+        fontSize : '30px',
+        delay: .3
+      })
+    })
 
-            </div>
+
+  },[])
+
+return (
+
+    <div className='main w-full  h-screen bg-[#1c1c1c]'>
+          
+           <div id='cur' className='hidden md:hidden sm:hidden lg:block w-[30px] h-[30px] z-[999] bg-[#b617ff3d] absolute rounded-full'></div>
+           
+        <div id='card' className='rounded-[50px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[90%] h-[85%] lg:w-[28%] lg:h-[fit-content] bg-[#c4c2c22b]'>
             <h1 className='w-[fit-content]  text-[40px] bg-[#a85ca86b] px-3 py-1 rounded-b-[30px] text-purple-300 m-auto '>Login</h1>
 
             <form className='w-full py-20 flex flex-col '>
               <input className='w-[270px] lg:w-[270px] font-semibold uppercase h-[50px] bg-slate-200 text-zinc-900 text-center text-2xl m-auto rounded-[30px] outline-none' type="text" placeholder='Username' />    
               <input className='mt-[30px] w-[270px] lg:w-[270px] font-semibold uppercase h-[50px] bg-slate-200 text-zinc-900 text-center text-2xl m-auto rounded-[30px] outline-none' type="password" placeholder='Password' />
-              <button className='mt-[30px] w-[270px] font-semibold uppercase h-[50px] bg-green-500 text-black text-center text-2xl m-auto rounded-[30px]'>SUBMIT</button>
+              <button id='but' className='mt-[30px] w-[270px] font-semibold uppercase h-[50px] bg-green-500 text-black text-center text-2xl m-auto rounded-[30px]'>SUBMIT</button>
               <a href="#">
-                <h1 className='mt-[20px] text-[25px] text-purple-300 text-center'>Forgot Password ?</h1>
+                <h1 className='mt-[20px] text-[25px] text-purple-300 text-center'>Forgot Password ? <br /> or <a href="">Sign Up</a></h1>
               </a>
             </form>
 
