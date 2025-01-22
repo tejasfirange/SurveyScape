63fcf3905d10a3d414795186055fd954ece3295f
Sairaj <ekbotesairaj640@gmail.com>
2025-01-22 02:10:20 -0800

    admin sign up done frontend only

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Adlogin from './components/Admin/Adlogin'
 import Landing from './components/Landing'
+import Adsign from './components/Adsign'
 function App() {
   return (
     <div>
-      <Adlogin />
+      <Adsign />
+      {/* <Adlogin /> */}
       {/* <Landing/> */}
       
     </div>
